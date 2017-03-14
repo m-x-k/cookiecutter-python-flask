e7af9d47b1735916305a8b7601c8f9bc79624710
kellyma <martin.kelly@ros.gov.uk>
2017-03-14 12:45:30 +0000

    jinja2 templates

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template, redirect
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.jinja2')
 
 if __name__ == '__main__':
     app.run()
